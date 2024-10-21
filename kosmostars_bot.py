817f9fae7f68e7c43e15bb51c5cb729420ba0282
Izdator <evarborisov@gmail.com>
2024-10-21 20:57:30 +0300

    Update kosmostars_bot.py
    publish photo to Telegram channel.

--- kosmostars_bot.py
+++ kosmostars_bot.py
@@ -7,3 +7,6 @@ print(bot.get_me())
 chat_id = '-1002280010724'  # Замените на настоящий ID чата
 
 bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
+
+with open('C:\\Users\\79647\\Desktop\\Upload_Space_Photos_To_Telegram\\images\\nasa_apod_20240922.jpg', 'rb') as file:
+    bot.send_document(chat_id=chat_id, document=file)
