f894c6864b990edcdea39bc57a1f3a842e72a642
Izdator <evarborisov@gmail.com>
2024-10-25 15:24:41 +0300

    Update kosmostars_bot.py
    Wraps the code

--- kosmostars_bot.py
+++ kosmostars_bot.py
@@ -1,18 +1,27 @@
 import telegram
 import os
-
 from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 token = os.environ.get('TELEGRAM_TOKEN')
 
+
 bot = telegram.Bot(token)
 
-print(bot.get_me())
 
-chat_id = '-1002280010724'  # Замените на настоящий ID чата
+def main():
+    print(bot.get_me())
 
-bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
+    chat_id = '-1002280010724'
 
-with open('C:\\Users\\79647\\Desktop\\Upload_Space_Photos_To_Telegram\\images\\nasa_apod_20240922.jpg', 'rb') as file:
-    bot.send_document(chat_id=chat_id, document=file)
+    bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
+
+    with open('C:\\Users\\79647\\Desktop\\Upload_Space_Photos_To_Telegram\\images\\nasa_apod_20240922.jpg', 'rb') as file:
+        bot.send_document(chat_id=chat_id, document=file)
+
+
+if __name__ == "__main__":
+    main()
