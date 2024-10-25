1bee0fa305ac9edcad74fb2dd51896f21610ab6e
Izdator <evarborisov@gmail.com>
2024-10-25 15:41:01 +0300

    Update kosmostars_bot.py
    Absolute path removed

--- kosmostars_bot.py
+++ kosmostars_bot.py
@@ -2,23 +2,21 @@ import telegram
 import os
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
-
 token = os.environ.get('TELEGRAM_TOKEN')
-
 chat_id = os.environ.get('CHAT_ID')
-
 
 bot = telegram.Bot(token)
 
 
 def main():
-
     bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
 
-    with open('C:\\Users\\79647\\Desktop\\Upload_Space_Photos_To_Telegram\\images\\nasa_apod_20240922.jpg', 'rb') as file:
+    relative_path = 'images/nasa_apod_20240922.jpg'
+    file_path = os.path.join(os.getcwd(), relative_path)
+
+    with open(file_path, 'rb') as file:
         bot.send_document(chat_id=chat_id, document=file)
 
 
