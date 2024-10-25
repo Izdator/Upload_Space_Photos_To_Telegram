14cf416e4278fdfa617a75c4954d7718c59972a9
Izdator <evarborisov@gmail.com>
2024-10-25 15:21:41 +0300

    Update earth_polychromatic_imaging_camera.py
    Added error protection in get parameters

--- earth_polychromatic_imaging_camera.py
+++ earth_polychromatic_imaging_camera.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta
 import os
-from urllib.parse import urlparse, unquote
-
+import urllib.parse
 from dotenv import load_dotenv
 import requests
 
@@ -13,18 +12,31 @@ def capture_earth_polychromatic_imaging_camera(api_key, desired_count=10):
     image_urls = []
     date_today = datetime.now()
 
+    if not api_key:
+        raise ValueError("API key is missing or invalid.")
+
     while len(image_urls) < desired_count:
         params = {
             'api_key': api_key,
         }
-        response = requests.get(base_url, params=params)
-        response.raise_for_status()
-        epic_data = response.json()
+        try:
+            response = requests.get(base_url, params=params)
+            response.raise_for_status()
+            epic_data = response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"Error during API request: {e}")
+            break
+        except json.JSONDecodeError:
+            print("Error decoding the JSON response.")
+            break
 
         for image_data in epic_data:
             if 'image' in image_data:
-                image_url = (f"https://api.nasa.gov/EPIC/archive/natural/{image_data['date'][:10].replace('-', '/')}"
-                             f"/png/{image_data['image']}.png?api_key={api_key}")
+                # Формируем базовый URL для изображения
+                image_base_url = f"https://api.nasa.gov/EPIC/archive/natural/{image_data['date'][:10].replace('-', '/')}/png/{image_data['image']}.png"
+
+                # Кодируем параметры GET
+                image_url = f"{image_base_url}?{urllib.parse.urlencode(params)}"
                 image_urls.append(image_url)
 
                 if len(image_urls) >= desired_count:
@@ -49,5 +61,6 @@ def capture_earth_polychromatic_imaging_camera(api_key, desired_count=10):
 
 
 if __name__ == "__main__":
+    load_dotenv()
     api_key = os.environ.get('NASA_TOKEN')
     urls_epic = capture_earth_polychromatic_imaging_camera(api_key)
