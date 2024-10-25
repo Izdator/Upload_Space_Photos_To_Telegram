28f7c53a95ddda389b186fa88eea4461ac1e71a5
Izdator <evarborisov@gmail.com>
2024-10-25 15:46:51 +0300

    Update posts_photos_automatically.py
    file_list replaced with file_names throughout the code

--- posts_photos_automatically.py
+++ posts_photos_automatically.py
@@ -16,10 +16,10 @@ def main():
     args = parser.parse_args()
     time_publication = args.publication_frequency * 3600
 
-    file_list = [f for f in os.listdir('images/') if os.path.isfile(os.path.join('images/', f))]
-    random.shuffle(file_list)
+    file_names = [f for f in os.listdir('images/') if os.path.isfile(os.path.join('images/', f))]
+    random.shuffle(file_names)
 
-    for file_name in file_list:
+    for file_name in file_names:
         file_path = os.path.join('images/', file_name)
         with open(file_path, 'rb') as file:
             bot.send_document(chat_id=chat_id, document=file)
