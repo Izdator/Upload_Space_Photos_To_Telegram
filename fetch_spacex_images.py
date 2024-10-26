6010dcfafe26644dd8f14f675b80c28b817504b3
Izdator <evarborisov@gmail.com>
2024-10-26 16:12:57 +0300

    Update fetch_spacex_images.py
    Added script description

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -5,7 +5,6 @@ import requests
 
 from general_functions import download_picture
 
-
 def fetch_spacex_last_launch(launch_url):
     response_spacex = requests.get(launch_url)
     response_spacex.raise_for_status()
@@ -15,11 +14,10 @@ def fetch_spacex_last_launch(launch_url):
         image_name = r'images/spacex_{n}.jpg'.format(n=image_number)
         download_picture(image_url, image_name)
 
-
 if __name__ == "__main__":
     os.makedirs('images', mode=0o777, exist_ok=True)
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Fetches the latest SpaceX launch images.")
     parser.add_argument('--launch_id', type=str, help='Launch ID of the SpaceX launch', default='latest')
     args = parser.parse_args()
 
