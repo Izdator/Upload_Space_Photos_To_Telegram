1e8aec89e5bbbd30a126e87204b4ad770786260b
Izdator <evarborisov@gmail.com>
2024-10-26 16:16:20 +0300

    Update fetch_spacex_images.py
    Added json requests

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -1,18 +1,19 @@
 import argparse
-import json
 import os
 import requests
 
 from general_functions import download_picture
 
+
 def fetch_spacex_last_launch(launch_url):
     response_spacex = requests.get(launch_url)
     response_spacex.raise_for_status()
-    launch_content = json.loads(response_spacex.text)
+    launch_content = response_spacex.json()
     image_urls = launch_content['links']['flickr']['original']
     for image_number, image_url in enumerate(image_urls):
         image_name = r'images/spacex_{n}.jpg'.format(n=image_number)
         download_picture(image_url, image_name)
+
 
 if __name__ == "__main__":
     os.makedirs('images', mode=0o777, exist_ok=True)
