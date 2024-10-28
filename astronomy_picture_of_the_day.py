8ed4c3272db65a549d03137cb62a5ea2b9b92d11
Izdator <evarborisov@gmail.com>
2024-10-29 02:01:23 +0300

    the number of network requests has been reduced

--- astronomy_picture_of_the_day.py
+++ astronomy_picture_of_the_day.py
@@ -1,41 +1,37 @@
 from datetime import datetime, timedelta
 import json
 import os
-from urllib.parse import urlparse, unquote
-
-from dotenv import load_dotenv
 import requests
-
+from dotenv import load_dotenv
 from general_functions import download_picture, detect_file_extension
 
 
-def get_astronomy_picture_of_the_day(api_key, count=30):
+def get_astronomy_picture_of_the_day(api_key, days=1):
     base_url = "https://api.nasa.gov/planetary/apod"
     image_urls = []
-    date_today = datetime.now()
 
     os.makedirs('images', exist_ok=True)
 
-    dates = [(date_today - timedelta(days=i)).strftime('%Y-%m-%d') for i in range(count)]
+    date_query = (datetime.now() - timedelta(days=days)).strftime('%Y-%m-%d')
 
-    for date in dates:
-        params = {
-            'api_key': api_key,
-            'date': date,
-        }
-        response = requests.get(base_url, params=params)
-        response.raise_for_status()
-        apod_content = response.json()
+    params = {
+        'api_key': api_key,
+        'date': date_query,
+    }
 
-        if 'url' in apod_content:
-            image_urls.append(apod_content['url'])
-            image_extension = detect_file_extension(apod_content['url'])
-            image_name = f'images/nasa_apod_{date.replace("-", "")}{image_extension}'
+    response = requests.get(base_url, params=params)
+    response.raise_for_status()
+    apod_content = response.json()
 
-            try:
-                download_picture(apod_content['url'], image_name)
-            except (requests.exceptions.RequestException, IOError) as e:
-                print(f"Error downloading image from {apod_content['url']}: {e}")
+    if 'url' in apod_content:
+        image_urls.append(apod_content['url'])
+        image_extension = detect_file_extension(apod_content['url'])
+        image_name = f'images/nasa_apod_{date_query.replace("-", "")}{image_extension}'
+
+        try:
+            download_picture(apod_content['url'], image_name)
+        except (requests.exceptions.RequestException, IOError) as e:
+            print(f"Error downloading image from {apod_content['url']}: {e}")
 
     return image_urls
 
@@ -43,4 +39,6 @@ def get_astronomy_picture_of_the_day(api_key, count=30):
 if __name__ == "__main__":
     load_dotenv()
     api_key = os.environ['NASA_TOKEN']
-    urls_apod = get_astronomy_picture_of_the_day(api_key)
+
+    days_to_fetch = 30
+    urls_apod = get_astronomy_picture_of_the_day(api_key, days=days_to_fetch)
--- earth_polychromatic_imaging_camera.py
+++ earth_polychromatic_imaging_camera.py
@@ -3,7 +3,6 @@ import os
 import json
 from dotenv import load_dotenv
 import requests
-
 from general_functions import download_picture
 
 
@@ -12,38 +11,34 @@ def capture_earth_polychromatic_imaging_camera(api_key, start_date, desired_coun
     image_urls = []
 
     date_today = datetime.strptime(start_date, '%Y-%m-%d')
-    dates = [(date_today - timedelta(days=i)).strftime('%Y-%m-%d') for i in range(desired_count)]
+    target_date = date_today - timedelta(days=desired_count)
 
     os.makedirs('images', exist_ok=True)
 
-    for date in dates:
-        params = {
-            'api_key': api_key,
-        }
+    params = {
+        'api_key': api_key,
+    }
 
-        try:
-            response = requests.get(base_url, params=params)
-            response.raise_for_status()
-            epic_content = response.json()
-        except requests.exceptions.RequestException as e:
-            print(f"Error during API request: {e}")
-            break
-        except json.JSONDecodeError:
-            print("Error decoding the JSON response.")
-            break
+    try:
+        response = requests.get(base_url, params=params)
+        response.raise_for_status()
+        epic_content = response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error during API request: {e}")
+        return []
+    except json.JSONDecodeError:
+        print("Error decoding the JSON response.")
+        return []
 
-        for image_content in epic_content:
-            if 'image' in image_content:
-                image_base_url = f"https://api.nasa.gov/EPIC/archive/natural/{image_content['date'][:10].replace('-', '/')}/png/{image_content['image']}.png"
-
+    for image_content in epic_content:
+        if 'image' in image_content:
+            image_date = image_content['date'][:10]  # Получаем дату в формате YYYY-MM-DD
+            if image_date == target_date.strftime('%Y-%m-%d'):
+                image_base_url = f"https://api.nasa.gov/EPIC/archive/natural/{image_date.replace('-', '/')}/png/{image_content['image']}.png"
                 image_url = f"{image_base_url}?api_key={params['api_key']}"
                 image_urls.append(image_url)
-
                 if len(image_urls) >= desired_count:
                     break
-
-        if len(image_urls) >= desired_count:
-            break
 
     for image_number, image_url in enumerate(image_urls):
         image_name = f'images/nasa_epic_{image_number}.png'
@@ -64,4 +59,5 @@ if __name__ == "__main__":
         raise ValueError("API key is missing or invalid.")
 
     start_date = '2023-09-22'
-    urls_epic = capture_earth_polychromatic_imaging_camera(api_key, start_date)
+    desired_days = 10
+    urls_epic = capture_earth_polychromatic_imaging_camera(api_key, start_date, desired_count=desired_days)
