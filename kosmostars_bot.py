9574290628413fe8423f7af3870c0fc310eeb9b6
Izdator <evarborisov@gmail.com>
2024-10-28 15:40:24 +0300

    bugs fixed
    bugs fixed

--- kosmostars_bot.py
+++ kosmostars_bot.py
@@ -1,9 +1,10 @@
 import telegram
 import os
+import argparse
 from dotenv import load_dotenv
 
 
-def main():
+def main(file_path):
     load_dotenv()
 
     token = os.environ.get('TELEGRAM_TOKEN')
@@ -12,12 +13,14 @@ def main():
     bot = telegram.Bot(token)
     bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
 
-    relative_path = 'images/nasa_apod_20240922.jpg'
-    file_path = os.path.join(os.getcwd(), relative_path)
-
     with open(file_path, 'rb') as file:
         bot.send_document(chat_id=chat_id, document=file)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Send a file to a Telegram chat.")
+    parser.add_argument('file_path', type=str, help='The path to the file to be sent.')
+
+    args = parser.parse_args()
+
+    main(args.file_path)
